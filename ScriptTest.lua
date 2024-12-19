8de11ec1d95365fe4617bad101a10bff7cee888a
ForceMJK <forceopbr@gmail.com>
2024-12-19 01:19:16 -0300

    Update ScriptTest.lua

--- ScriptTest.lua
+++ ScriptTest.lua
@@ -9,8 +9,8 @@ local HTTPService = game:GetService("HttpService")
 local Library = {
 	Themes = {
 		Legacy = {
-			Main = Color3.fromHSV(50/360, 50/255, 55/255),
-			Secondary = Color3.fromHSV(50/360, 50/255, 55/255),
+			Main = Color3.fromHSV(20/360, 20/255, 25/255),
+			Secondary = Color3.fromHSV(30/360, 30/255, 35/255),
 			Tertiary = Color3.fromHSV(0/360, 153/255, 230/255),
 
 			StrongText = Color3.fromHSV(0, 0, 0),		
